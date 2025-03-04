8d8785eebfe13a27d2a62ee917def71879e458ff
malekit01 <malekit01@hotmail.com>
2025-03-04 22:18:22 +0100

    Update extractorLinks.py

--- extractorLinks.py
+++ extractorLinks.py
@@ -3,8 +3,8 @@ import xml.etree.ElementTree as ET
 import re
 
 # Directorios
-xml_dir = "/home/javier/Escritorio/ciencia_abierta/XML_articulos"
-output_dir = "/home/javier/Escritorio/ciencia_abierta/links_articulos"
+xml_dir = "/home/javi/Escritorio/ciencia_abierta/resultados/XML_articulos"
+output_dir = "/home/javi/Escritorio/ciencia_abierta/resultados/links_articulos"
 
 # Asegurar que el directorio de salida existe
 if not os.path.exists(output_dir):
